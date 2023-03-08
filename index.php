a9806051b6af1c69467280f38ea41a774d95c9e6
ttiennot <113522386+ttiennot@users.noreply.github.com>
2023-03-08 17:37:08 +0100

    exo10js

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exo 9.3</title>
+    <title>exo 10</title>
+    <link rel="stylesheet" href="main.css">
 </head>
-
 <body>
-    <div class="lesdivs">Coucou</div>
-    <div class="lesdivs">Coucou</div>
-    <div class="lesdivs">Coucou</div>
+    <div id="toto">coucou</div>
+    
 </body>
 <script src="main.js"></script>
 <script type="text/javascript">
-    modify();
+    Survole(evenement);
+    SurvolePas(evenement);
 </script>
 
 </html>
--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-function modify() {
-    var NewDiv = document.getElementsByClassName("lesdivs");
-    for(var i = 0; i < NewDiv.length; i++){
-        NewDiv[i].innerHTML = "Hello";
-    }    
+maDiv = document.getElementById("toto")
+maDiv.addEventListener("mouseover", Survole);
+maDiv.addEventListener("mouseout", SurvolePas);
+maDiv.addEventListener("click", Click);
+
+function Survole(evenement){
+    evenement.target.innerHTML = "survolé";
+    evenement.target.className = "class";
+}
+
+function SurvolePas(evenement){
+    evenement.target.innerHTML = "pas survolé";
+    evenement.target.className = "class2";
+}
+
+function Click(evenement){
+    evenement.target.innerHTML = "cliqué";
+    evenement.target.className = "class3";
 }
